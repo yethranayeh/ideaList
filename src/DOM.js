5b01ec93809cce52aedecbfe337ce91a9dea5826
yethranayeh <aktasalper99@gmail.com>
2022-01-07 13:32:04 +0300

    added hamburger menu icon

--- src/DOM.js
+++ src/DOM.js
@@ -5,5 +5,40 @@ export { DOM as default };
 const DOM = {
 	init: function () {
 		this.body = document.querySelector("body");
-	}
+		this.contentArea = this.body.querySelector("#content");
+		this.contentArea.appendChild(this.navbar.self);
+	},
+	initInterface: function () {
+		return;
+	},
+	navbar: (function () {
+		const navbar = document.createElement("nav");
+
+		// Hamburger
+		const hamburger = (function () {
+			const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			svg.classList.add("ham", "hamRotate", "ham4");
+			svg.setAttribute("viewBox", "0 0 100 100");
+			svg.setAttribute("width", "80");
+
+			// Hamburger paths
+			const classes = ["top", "middle", "bottom"];
+			const dAttr = [
+				"m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20",
+				"m 70,50 h -40",
+				"m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+			];
+			for (let i = 0; i < 3; i++) {
+				let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+				path.classList.add("line", classes[i]);
+				path.setAttribute("d", dAttr[i]);
+				svg.appendChild(path);
+			}
+			return svg;
+		})();
+
+		navbar.appendChild(hamburger);
+
+		return { self: navbar, hamburger: hamburger };
+	})()
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 /** @format */
 
+import "./reset.css";
+import "./style.css";
 import PubSub from "pubsub-js";
 import App from "./app.js";
 import DOM from "./DOM.js";
 
 App.initTodoList();
 DOM.init();
+
+DOM.navbar.hamburger.addEventListener("click", (e) => {
+	DOM.navbar.hamburger.classList.toggle("active");
+	if (DOM.navbar.hamburger.classList.contains("active")) {
+		console.count("active");
+	} else {
+		console.count("not active");
+	}
+});
