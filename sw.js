d58f6a4c9287f4e97137cd82f63a6b24888cfe9c
yethranayeh <aktasalper99@gmail.com>
2022-01-24 00:07:36 +0300

    updated service worker and webpack config

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
 const staticCacheName = "static-v1";
 const assets = [
 	"/",
-	"/index.html",
-	"/main.js",
-	"/icons/favicon.ico",
+	"./index.html",
+	"./main.js",
+	"./icons/favicon.ico",
 	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
 	"https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
 ];
